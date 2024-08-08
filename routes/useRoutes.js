341035ab8ab0f4f236f300c88490bdf569010ea5
StephenAdedayo <alustephenadedayo@gmail.com>
2024-08-08 16:19:43 +0100

    route

--- routes/useRoutes.js
+++ routes/useRoutes.js
@@ -2,6 +2,6 @@ const express = require('express')
 const {register} = require('../controllers/useControllers')
 const router = express.Router()
 
-router.post('/user/register', register)
+router.post('/user/registerUser', register)
 
 module.exports = router
